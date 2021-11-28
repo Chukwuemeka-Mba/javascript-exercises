067b9664f5b411402b70649fd1f544fe055da23d
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2021-11-28 11:53:06 -0500

    finished two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+// function repeatString(string, num) {
+//     let stringRepeat = "";
+//     while(num > 0){
+//         stringRepeat += string;
+//         num--
+//     }
+//     return stringRepeat
+// };
 
-};
+// repeatString("James", 8)
+
+
+function repeatString(str, num){
+    if(num > 0){
+        return str.repeat(num);
+    }else{
+        return "";
+    }
+}
+
+repeatString("Jonas", 9);
 
 // Do not edit below this line
 module.exports = repeatString;
