27ef70a6060af03fbf38521c3b07aee51126ac67
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-01-18 16:34:50 -0500

    cleaning up things

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function(str) {
+function reverseString(str) {
+    var strSplit = str.split("");
+    var strReverse = strSplit.reverse();
+    var strJoin = strReverse.join("");
 
+    return strJoin
 };
 
+reverseString('Captain');
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
-
+    let array = [1, 2, 3, 4];
+    let args = 3;
+const removeFromArray = function(array, args) {
+    
+    let newArray = array.pop(args);
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
